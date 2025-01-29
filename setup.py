1122dd4f8f016e9ee869e9bc8e65e80b5016c80b
meekele_b <48720814+MicheleBarsotti@users.noreply.github.com>
2025-01-29 12:39:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,37 +27,9 @@ INSTALL_REQUIRES = [
     "numpy>=1.24.4",
 ]
 
-EXTRAS_REQUIRE = {
-    "machine_learning": [
-        "scikit-learn>=0.23.2",
-        "torch>=1.11,<1.13",
-        "torchvision>=0.12.0",
-        "torchaudio>=0.7.0",
-        "einops>=0.3.0",
-        "pyyaml>=5.4.1",
-    ],
-    "signal_processing": ["scipy>=1.5.0"],
-    "testing": ["more-itertools>=8.7.0", "pytest>=6.2.2", "pytest-mock>=3.5.1"],
-    "eeg": ["mne>=1.0.2", "scipy>=1.5.0", "pyxdf>=1.16.3", "mnelab>=0.6.3"],
-    "classification": [
-        "scikit-learn>=0.23.2",
-        "pyriemann>=0.2.6",
-        "torch>=1.11,<1.13",
-        "einops>=0.3.0",
-        "pyyaml>=5.4.1",
-        "sktime>=0.6.1",
-        "Braindecode==0.4.85",
-    ],
+EXTRAS_REQUIRE = {    
     "utilities": ["fire>=0.4.0", "tqdm>=4.59.0", "plotly>=4.11.0"],
 }
-EXTRAS_REQUIRE["development"] = list(set(sum(EXTRAS_REQUIRE.values(), [])))
-
-EXTRAS_REQUIRE["demo"] = EXTRAS_REQUIRE["classification"] + EXTRAS_REQUIRE["signal_processing"]
-
-_BAD_REQS = ("mne", "mnelab", "Braindecode", "pyxdf")
-EXTRAS_REQUIRE["dev_lite"] = [
-    req for req in EXTRAS_REQUIRE["development"] if not req.startswith(_BAD_REQS)
-]
 
 
 setuptools.setup(
